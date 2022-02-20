641b0eb5debe850ac52c8c574a1ec047eea2ca15
Alfie Mills <alfie.mills@outlook.com>
2022-02-20 17:59:54 +0000

    Adding proxy support and logging

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-
 const tools = require('./lib/helperFunctions')
 const key = process.env.apikey
 const port = 3000
 
+app.set('trust proxy', true)
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');	
+	res.send('Unauthorized Request');	
 })
 
 app.get('/notify', function(req, res) {
-	res.send(req.query)
+	res.send('Done.')
+	
+	tools.log(JSON.stringify({
+		"ip": req.ip,
+		"headers": req.headers,
+		"query": req.query,
+	}), 'access')
+
 	tools.callTrigger( 'event', JSON.stringify(req.query), key )
+
 });
 
-
 app.listen(port, () => {
-	console.log(`Notify API listening on port ${port}`)
+	tools.log(`Notify API listening on port ${port}`)
 })
